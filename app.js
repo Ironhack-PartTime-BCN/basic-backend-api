9915dd3d984e50447c54a36cb1850d1a4cc46969
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2019-04-02 16:21:43 +0200

    🎨 Format by prettier
    Format code by prettier

--- app.js
+++ app.js
@@ -1,73 +1,72 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const cors = require('cors');
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const cors = require('cors')
 
-const phones = require('./routes/phones');
+const phones = require('./routes/phones')
 
-require('dotenv').config();
-
+require('dotenv').config()
 
 mongoose
-  .connect(
-    process.env.DATABASE,
-    { useNewUrlParser: true }
-  )
+  .connect(process.env.DATABASE, {useNewUrlParser: true})
   .then(() => {
-    console.log(`Connected to ${process.env.DATABASE} database`);
+    // eslint-disable-next-line no-console
+    console.log(`Connected to ${process.env.DATABASE} database`)
   })
   .catch(error => {
-    console.log(error);
-    mongoose.connection.close();
-  });
+    mongoose.connection.close()
+    throw error
+  })
 
-const app = express();
+const app = express()
 
-
-app.use(cors({
-  credentials: true,
-  origin: ['http://localhost:3000']
-}));
-
-app.use(session({
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // 1 day
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
   }),
-  secret: 'some-string',
-  resave: true,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000
-  },
-}));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+)
 
-app.use('/api', phones);
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+    secret: 'some-string',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  }),
+)
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+
+app.use('/api', phones)
 
 app.use((req, res, next) => {
   res.status(404)
-  res.json({ code: 'not found' });
-});
+  res.json({code: 'not found'})
+})
 
 app.use((err, req, res, next) => {
   // always log the error
-  console.error('ERROR', req.method, req.path, err);
+  console.error('ERROR', req.method, req.path, err)
 
   // only render if the error ocurred before sending the response
   if (!res.headersSent) {
     res.status(500)
-    res.json({ code: 'unexpected' });
+    res.json({code: 'unexpected'})
   }
-});
+})
 
-module.exports = app;
+module.exports = app
